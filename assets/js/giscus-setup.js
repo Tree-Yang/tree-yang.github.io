5b3fa496a1570355f72baf2e1921022e6faab90f
Tree-Yang <yangjiashu2015@outlook.com>
2026-05-02 06:10:57 +0000

    Deploying to gh-pages from @ Tree-Yang/tree-yang.github.io@326a1c9df6056591a88fa8a03907bbc65430a2ab 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Tree-Yang/yangjs.github.io",
+    "data-repo": "Tree-Yang/tree-yang.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
